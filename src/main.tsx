f925f92188f99bdeefddc5c8dece9de38f009991
Igor Storozhuk <igorstorozhuk@MacBook-Pro-Igor.local>
2023-12-05 11:47:29 +0200

    browser msw init

--- src/main.tsx
+++ src/main.tsx
@@ -11,12 +11,26 @@ const store = setupStore();
 
 const persistor = persistStore(store)
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-)
+async function enableMocking() {
+  //if (process.env.NODE_ENV !== 'development') {
+    //return
+  //}
+ 
+  const { worker } = await import('./mocks/browser')
+ 
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+  )
+})
